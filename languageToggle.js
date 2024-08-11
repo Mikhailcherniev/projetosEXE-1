6cab5de7e551a704673c047a2ee1ac0dc78a00f8
IIIgorMoura <133662971+IIIgorMoura@users.noreply.github.com>
2024-08-11 03:18:05 -0300

    base traducao e estrutura

--- languageToggle.js
+++ languageToggle.js
@@ -1,22 +1,63 @@
+let currentLanguage = 'en';
+
+function toggleLanguage() {
+    if (currentLanguage === 'en') {
+        Escolher('pt');
+        document.getElementById('txtLanguage').innerText = 'Português';
+        document.getElementById('iconLanguage').src = './assets/icons/iconPT.png';
+        currentLanguage = 'pt';
+    } else {
+        Escolher('en');
+        document.getElementById('txtLanguage').innerText = 'English';
+        document.getElementById('iconLanguage').src = './assets/icons/iconEN.png';
+        currentLanguage = 'en';
+    }
+}
+
 function Escolher(idioma) {
     fetch(`txt_${idioma}.json`)
         .then(resposta => resposta.json())
         .then(texto => {
-            document.getElementById('txtLanguageTest').innerText = texto.txtLanguageTest;
-            // document.getElementById('paragrafo2').innerText = texto.paragrafo2;
+            // Primeiro, você pode definir o texto do título principal, exceto a parte que está no <span>
+            let landingPageTitle = texto.landingPageTitle.split("{span}")[0]; // Texto antes do <span>
+            let landingPageTitleAfterSpan = texto.landingPageTitle.split("{span}")[1]; // Texto depois do <span>
+            
+            document.getElementById('landingPageTitle').childNodes[0].nodeValue = landingPageTitle; // Define o texto antes do <span>
+            document.getElementById('DlandingPageTitle').innerText = texto.DlandingPageTitle; // Define o texto dentro do <span>
+            document.getElementById('landingPageTitle').childNodes[2].nodeValue = landingPageTitleAfterSpan; // Define o texto depois do <span>
 
-            // document.getElementById('pergunta1').innerText = texto.pergunta1;
-            // document.getElementById('resposta1').innerText = texto.resposta1;
 
-            // document.getElementById('pergunta2').innerText = texto.pergunta2;
-            // document.getElementById('resposta2').innerText = texto.resposta2;
 
-            // document.getElementById('pergunta3').innerText = texto.pergunta3;
-            // document.getElementById('resposta3').innerText = texto.resposta3;
+            let aboutUsTitle = texto.aboutUsTitle.split("{span}")[0];
+            // Atualiza o texto do elemento <h2> antes do <span>
+            document.getElementById('aboutUsTitle').childNodes[0].nodeValue = aboutUsTitle;
+            // Atualiza o texto dentro do <span>
+            document.getElementById('DaboutUsTitle').innerText = texto.DaboutUsTitle;
 
-            // document.getElementById('pergunta4').innerText = texto.pergunta4;
-            // document.getElementById('resposta4').innerText = texto.resposta4;
+            let ourArtTitle = texto.ourArtTitle.split("{span}")[0];
+            document.getElementById('ourArtTitle').childNodes[0].nodeValue = ourArtTitle;
+            document.getElementById('DourArtTitle').innerText = texto.DourArtTitle;
+
+            let ourCoursesTitle = texto.ourCoursesTitle.split("{span}")[0];
+            document.getElementById('ourCoursesTitle').childNodes[0].nodeValue = ourCoursesTitle;
+            document.getElementById('DourCoursesTitle').innerText = texto.DourCoursesTitle;
+
+            let nextEventsTitle = texto.nextEventsTitle.split("{span}")[0];
+            document.getElementById('nextEventsTitle').childNodes[0].nodeValue = nextEventsTitle;
+            document.getElementById('DnextEventsTitle').innerText = texto.DnextEventsTitle;
+
+            let ourLocationTitle = texto.ourLocationTitle.split("{span}")[0];
+            document.getElementById('ourLocationTitle').childNodes[0].nodeValue = ourLocationTitle;
+            document.getElementById('DourLocationTitle').innerText = texto.DourLocationTitle;
+
+            let seeMoreTitle = texto.seeMoreTitle.split("{span}")[0];
+            document.getElementById('seeMoreTitle').childNodes[0].nodeValue = seeMoreTitle;
+            document.getElementById('DseeMoreTitle').innerText = texto.DseeMoreTitle;
+
+            let workWUTitle = texto.workWUTitle.split("{span}")[0];
+            document.getElementById('workWUTitle').childNodes[0].nodeValue = workWUTitle;
+            document.getElementById('DworkWUTitle').innerText = texto.DworkWUTitle;
+
         })
-
-        .catch(error => console.error('Erro buscando textos:', error))
+        .catch(error => console.error('Erro ao carregar o arquivo de idioma:', error));
 }
